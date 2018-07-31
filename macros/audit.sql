bc7d10d7268852a94efd8b7ff1bf673a60b61063
David Wallace <davidwallace@Davids-MBP.envoy.hq>
2018-07-31 14:30:31 -0700

    add variables back in

--- macros/audit.sql
+++ macros/audit.sql
@@ -36,13 +36,17 @@
 {% endmacro %}
 
 
-{% macro create_audit_schema() %}
+{% macro create_audit_schema(run_dbt_logging=False) %}
+  {% if run_dbt_logging %}
     create schema if not exists {{ logging.get_audit_schema() }}
+  {% else %}
+    select 1
+  {% endif %}
 {% endmacro %}
 
 
-{% macro create_audit_log_table() %}
-
+{% macro create_audit_log_table(run_dbt_logging=False) %}
+  {% if run_dbt_logging %}
     create table if not exists {{ logging.get_audit_relation() }}
     (
        event_name       varchar(512),
@@ -51,17 +55,28 @@
        event_model      varchar(512),
        invocation_id    varchar(512)
     )
+  {% else %}
+    select 1
+  {% endif %}
 
 {% endmacro %}
 
 
-{% macro log_run_start_event() %}
+{% macro log_run_start_event(run_dbt_logging=False) %}
+  {% if run_dbt_logging %}
     {{logging.log_audit_event('run started')}}
+  {% else %}
+    select 1
+  {% endif %}
 {% endmacro %}
 
 
-{% macro log_run_end_event() %}
+{% macro log_run_end_event(run_dbt_logging=False) %}
+  {% if run_dbt_logging %}
     {{logging.log_audit_event('run completed')}}; commit;
+  {% else %}
+    select 1
+  {% endif %}
 {% endmacro %}
 
 
