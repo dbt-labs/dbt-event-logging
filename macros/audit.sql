2b730e8027f848ad7bca631529d8eefdb3332201
David Wallace <davidwallace@Davids-MBP.envoy.hq>
2018-07-31 11:22:53 -0700

    update project file and add s3 unload macro

--- macros/audit.sql
+++ macros/audit.sql
@@ -1,8 +1,8 @@
 {% macro get_audit_relation() %}
-    {%- set audit_table = 
+    {%- set audit_table =
         api.Relation.create(
-            identifier='dbt_audit_log', 
-            schema=target.schema~'_meta', 
+            identifier='dbt_audit_log',
+            schema=target.schema~'_meta',
             type='table'
         ) -%}
     {{ return(audit_table) }}
@@ -11,25 +11,25 @@
 
 {% macro get_audit_schema() %}
     {% set audit_table = logging.get_audit_relation() %}
-    {{ return(audit_table.include(schema=True, identifier=False)) }}    
+    {{ return(audit_table.include(schema=True, identifier=False)) }}
 {% endmacro %}
 
 
 {% macro log_audit_event(event_name, schema, relation) %}
 
     insert into {{ logging.get_audit_relation() }} (
-        event_name, 
-        event_timestamp, 
-        event_schema, 
+        event_name,
+        event_timestamp,
+        event_schema,
         event_model,
         invocation_id
-        ) 
-    
+        )
+
     values (
-        '{{ event_name }}', 
-        {{dbt_utils.current_timestamp_in_utc()}}, 
-        {% if variable != None %}'{{ schema }}'{% else %}null::varchar(512){% endif %}, 
-        {% if variable != None %}'{{ relation }}'{% else %}null::varchar(512){% endif %}, 
+        '{{ event_name }}',
+        {{dbt_utils.current_timestamp_in_utc()}},
+        {% if variable != None %}'{{ schema }}'{% else %}null::varchar(512){% endif %},
+        {% if variable != None %}'{{ relation }}'{% else %}null::varchar(512){% endif %},
         '{{ invocation_id }}'
         )
 
@@ -76,4 +76,12 @@
     {{logging.log_audit_event(
         'model deployment completed', this.schema, this.name
         )}}
+{% endmacro %}
+
+{% macro unload_to_s3(unload_to_s3=False) %}
+  {% if unload_to_s3 %}
+    unload ('select dil.* from dbt_spectrum_redshift.dbt_invocation_logs dil union select d.* from {{ logging.get_audit_relation() }} d') to 's3://redshift-dbt-logs/dbt_invocation_logs/dbt_invocation_logs_' iam_role '{{ env_var('DBT_IAM_ROLE') }}' manifest delimiter as ',' null as '' escape allowoverwrite {{log(modules.datetime.datetime.now().strftime('%H:%M:%S') ~ ' | Unload to s3 requested for dbt invocation logs.', info=True)}}
+  {% else %}
+    select 1
+  {% endif %}
 {% endmacro %}
