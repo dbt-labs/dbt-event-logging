2f1151d7658d40afb27598d8605ff79fec3fd31d
David Wallace <davidwallace@Davids-MBP.envoy.hq>
2018-08-06 11:30:58 -0700

    add in variable for run dbt logging boolean

--- macros/audit.sql
+++ macros/audit.sql
@@ -93,12 +93,14 @@
         )}}
 {% endmacro %}
 
-{% macro unload_to_s3(unload_to_s3=False) %}
+{% macro unload_to_s3(run_dbt_logging=False, unload_to_s3=False) %}
 
-  {% if unload_to_s3 %}
-    {{ logging.create_audit_schema(run_dbt_logging=True) }};
-    {{ logging.create_audit_log_table(run_dbt_logging=True) }};
-    unload ('select dil.* from dbt_spectrum_redshift.dbt_invocation_logs dil union select d.* from {{ logging.get_audit_relation() }} d') to 's3://redshift-dbt-logs/dbt_invocation_logs/dbt_invocation_logs_' iam_role '{{ var('dbt_iam_role') }}' manifest delimiter as ',' null as '' escape allowoverwrite
+  {% if run_dbt_logging %}
+    {% if unload_to_s3 %}
+      unload ('select dil.* from dbt_spectrum_redshift.dbt_invocation_logs dil union select d.* from {{ logging.get_audit_relation() }} d') to 's3://redshift-dbt-logs/dbt_invocation_logs/dbt_invocation_logs_' iam_role '{{ var('dbt_iam_role') }}' manifest delimiter as ',' null as '' escape allowoverwrite
+    {% else %}
+      select 1
+    {% endif %}
   {% else %}
     select 1
   {% endif %}
