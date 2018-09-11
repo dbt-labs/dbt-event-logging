12a292b178ea3ff40b40c0a45b9276cdb11f923b
David Wallace <davidwallace@Davids-MBP.envoy.hq>
2018-09-11 15:25:26 -0700

    new models for logging

--- models/stg_dbt_deployments.sql
+++ models/stg_dbt_deployments.sql
@@ -1,30 +1,18 @@
 with events as (
-
-    select * from {{ref('stg_dbt_audit_log')}}
-
+select *
+from {{ref('stg_dbt_audit_log')}}
 ),
 
 aggregated as (
-
-    select 
-    
-        invocation_id,
-    
-        min(case 
-            when event_name = 'run started' then event_timestamp 
-            end) as deployment_started_at,
-    
-        min(case 
-            when event_name = 'run completed' then event_timestamp 
-            end) as deployment_completed_at,
-            
-        count(distinct case 
-            when event_name ilike '%model%' then event_model 
-            end) as models_deployed
-    
-    from events
-    group by 1
-
+select
+  invocation_id,
+  min(case when event_name = 'run started' then event_timestamp end) as deployment_started_at,
+  min(case when event_name = 'run completed' then event_timestamp end) as deployment_completed_at,
+  datediff('seconds',deployment_started_at,deployment_completed_at) as deployment_time_seconds,
+  count(distinct case when event_name ilike '%model%' then event_model end) as models_deployed
+from events
+group by 1
 )
 
-select * from aggregated
+select *
+from aggregated
--- models/stg_dbt_model_deployments.sql
+++ models/stg_dbt_model_deployments.sql
@@ -1,33 +1,20 @@
 with events as (
-
-    select * from {{ref('stg_dbt_audit_log')}}
-
+select *
+from {{ref('stg_dbt_audit_log')}}
 ),
 
 aggregated as (
-
-    select 
-    
-        {{dbt_utils.surrogate_key(
-            'event_model', 
-            'invocation_id'
-            )}} as model_deployment_id,
-    
-        invocation_id,
-        event_model as model,
-    
-        min(case 
-            when event_name = 'model deployment started' then event_timestamp 
-            end) as deployment_started_at,
-    
-        min(case 
-            when event_name = 'model deployment completed' then event_timestamp 
-            end) as deployment_completed_at
-    
-    from events
-    where event_name ilike '%model%'
-    group by 1, 2, 3
-
+select
+  {{dbt_utils.surrogate_key('event_model', 'invocation_id')}} as model_deployment_id,
+  invocation_id,
+  event_model as model,
+  min(case when event_name = 'model deployment started' then event_timestamp end) as deployment_started_at,
+  min(case when event_name = 'model deployment completed' then event_timestamp end) as deployment_completed_at,
+  datediff('seconds',deployment_started_at,deployment_completed_at) as deployment_time_seconds
+from events
+where event_name ilike '%model%'
+group by 1, 2, 3
 )
 
-select * from aggregated
+select *
+from aggregated
