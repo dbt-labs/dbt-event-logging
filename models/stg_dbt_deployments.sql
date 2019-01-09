6d6a236f6c3ec98fc8589df147748e46ff7363dd
David Wallace <davidwallace@Davids-MBP.envoy.hq>
2019-01-09 09:21:23 -0800

    update models for new full refresh field

--- models/stg_dbt_deployments.sql
+++ models/stg_dbt_deployments.sql
@@ -9,9 +9,10 @@ select
   min(case when event_name = 'run started' then event_timestamp end) as deployment_started_at,
   min(case when event_name = 'run completed' then event_timestamp end) as deployment_completed_at,
   datediff('seconds',deployment_started_at,deployment_completed_at) as deployment_time_seconds,
-  count(distinct case when event_name ilike '%model%' then event_model end) as models_deployed
+  count(distinct case when event_name ilike '%model%' then event_model end) as models_deployed,
+  is_full_refresh
 from events
-group by 1
+group by invocation_id,is_full_refresh
 )
 
 select *
--- models/stg_dbt_model_deployments.sql
+++ models/stg_dbt_model_deployments.sql
@@ -8,12 +8,13 @@ select
   {{dbt_utils.surrogate_key('event_model', 'invocation_id')}} as model_deployment_id,
   invocation_id,
   event_model as model,
+  is_full_refresh,
   min(case when event_name = 'model deployment started' then event_timestamp end) as deployment_started_at,
   min(case when event_name = 'model deployment completed' then event_timestamp end) as deployment_completed_at,
   datediff('seconds',deployment_started_at,deployment_completed_at) as deployment_time_seconds
 from events
 where event_name ilike '%model%'
-group by 1, 2, 3
+group by 1, 2, 3, 4
 )
 
 select *
