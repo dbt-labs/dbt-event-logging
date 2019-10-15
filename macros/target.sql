43152b0e9857b1c710d31a4511723a033f50f690
James O'Hare <james.ohare@simplybusiness.co.uk>
2019-10-15 17:55:57 +0100

    tweaks

--- macros/target.sql
+++ macros/target.sql
@@ -1,4 +1,4 @@
-{% macro logging_target_create_audit_log_table() %}
+{% macro log_target_create_audit_log_table() %}
   {%- if target.name == 'prod' -%}
 
     {{ logging.create_audit_log_table() }}
@@ -7,28 +7,10 @@
 {% endmacro %}
 
 
-{% macro logging_target_create_audit_schema() %}
+{% macro log_target_create_audit_schema() %}
   {%- if target.name == 'prod' -%}
 
     {{ logging.create_audit_schema() }}
-
-  {%- endif -%}
-{% endmacro %}
-
-
-{% macro logging_target_start() %}
-  {%- if target.name == 'prod' -%}
-
-    {{ logging.log_run_start_event() }}
-
-  {%- endif -%}
-{% endmacro %}
-
-
-{% macro logging_target_end() %}
-  {%- if target.name == 'prod' -%}
-
-    {{ logging.log_run_end_event() }}
 
   {%- endif -%}
 {% endmacro %}
@@ -63,4 +45,30 @@
   {%- endif -%}
 {% endmacro %}
 
+
+{% macro log_run_target_start_event() %}
+  {%- if target.name == 'prod' -%}
+
+
+    {{logging.log_audit_event('run started')}}
+
+  {%- else -%}
+
+    select 1 as test
+
+  {%- endif -%}
+{% endmacro %}
+
+
+{% macro log_run_target_end_event() %}
+  {%- if target.name == 'prod' -%}
+
+    {{logging.log_audit_event('run completed')}}; commit;
+
+  {%- else -%}
+
+    select 1 as test
+
+  {%- endif -%}
+{% endmacro %}
 
