7d7df7caac48dc4c1ce3eac42aae883dc8030f37
James O'Hare <james.ohare@simplybusiness.co.uk>
2019-10-16 10:08:21 +0100

    rearrange

--- README.md
+++ README.md
@@ -20,8 +20,8 @@ There are three convenience models to make it easier to parse the event log data
 2. Include the following in your `dbt_project.yml` directly within your `models:` directive (making sure to handle indenting appropriately):
 
 ```YAML
-pre-hook: "{{ logging.log_model_target_start_event() }}"
-post-hook: "{{ logging.log_model_target_end_event() }}"
+pre-hook: "{{ logging.log_model_start_event() }}"
+post-hook: "{{ logging.log_model_end_event() }}"
 ```
 
 That's it! You'll now have a stream of events for all dbt invocations in your warehouse.
--- macros/audit.sql
+++ macros/audit.sql
@@ -25,7 +25,7 @@
         invocation_id
         )
 
-    values (
+    values (gs
         '{{ event_name }}',
         {{dbt_utils.current_timestamp_in_utc()}},
         {% if variable != None %}'{{ schema }}'{% else %}null::varchar(512){% endif %},
@@ -37,12 +37,14 @@
 
 
 {% macro create_audit_schema() %}
+  {%- if target.name == 'prod' -%}
     create schema if not exists {{ logging.get_audit_schema() }}
+  {%- endif -%}
 {% endmacro %}
 
 
 {% macro create_audit_log_table() %}
-
+  {%- if target.name == 'prod' -%}
     create table if not exists {{ logging.get_audit_relation() }}
     (
        event_name       varchar(512),
@@ -51,29 +53,57 @@
        event_model      varchar(512),
        invocation_id    varchar(512)
     )
-
+  {%- endif -%}
 {% endmacro %}
 
 
 {% macro log_run_start_event() %}
+  {%- if target.name == 'prod' -%}
     {{logging.log_audit_event('run started')}}
+
+      {%- else -%}
+
+    select 1 as return_value
+
+  {%- endif -%}
 {% endmacro %}
 
 
 {% macro log_run_end_event() %}
+  {%- if target.name == 'prod' -%}
     {{logging.log_audit_event('run completed')}}; commit;
+
+  {%- else -%}
+
+    select 1 as return_value
+
+  {%- endif -%}
 {% endmacro %}
 
 
 {% macro log_model_start_event() %}
+  {%- if target.name == 'prod' -%}
     {{logging.log_audit_event(
         'model deployment started', this.schema, this.name
         )}}
+
+  {%- else -%}
+
+    select 1 as return_value
+
+  {%- endif -%}
 {% endmacro %}
 
 
 {% macro log_model_end_event() %}
+  {%- if target.name == 'prod' -%}
     {{logging.log_audit_event(
         'model deployment completed', this.schema, this.name
         )}}
+
+  {%- else -%}
+
+    select 1 as return_value
+
+  {%- endif -%}
 {% endmacro %}
